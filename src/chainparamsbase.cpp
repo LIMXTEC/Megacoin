ec48c4f86df81ade4bd5779416c5be3fb3f1cb86
Chris <apfelbaum@email.de>
2018-06-22 13:45:02 +0200

    Change RPC Port

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -32,7 +32,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 8556;//rPort
+        nRPCPort = 7952;//rPort
     }
 };
 
@@ -44,7 +44,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 50332;
+        nRPCPort = 50732;
         strDataDir = "testnet3";
     }
 };
@@ -57,7 +57,7 @@ class CBaseRegTestParams : public CBaseChainParams
 public:
     CBaseRegTestParams()
     {
-        nRPCPort = 50332;
+        nRPCPort = 50732;
         strDataDir = "regtest";
     }
 };
