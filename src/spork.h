9e45f155683a18ca29f7036d20f12ca4d1158021
limxdev <limx_dev@pm.me>
2020-04-25 13:13:16 +0200

    Update spork.h

--- src/spork.h
+++ src/spork.h
@@ -24,53 +24,53 @@ class CSporkManager;
     Don't ever reuse these IDs for other sporks
     - This would result in old clients getting confused about which spork is for what
 */
-static const int SPORK_START                                        = 10001;
-static const int SPORK_END                                          = 10013;
+static const int SPORK_START                                                = 10001;
+static const int SPORK_END                                                  = 10013;
 // FXTC BEGIN
-static const int SPORK_MEGACOIN_START                                    = 94680010;
-static const int SPORK_MEGACOIN_END                                      = 94680033;
+static const int SPORK_MEGACOIN_START                                       = 94680010;
+static const int SPORK_MEGACOIN_END                                         = 94680033;
 // FXTC END
 
-static const int SPORK_2_INSTANTSEND_ENABLED                        = 10001;
-static const int SPORK_3_INSTANTSEND_BLOCK_FILTERING                = 10002;
-static const int SPORK_5_INSTANTSEND_MAX_VALUE                      = 10004;
-static const int SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT             = 10007;
-static const int SPORK_9_SUPERBLOCKS_ENABLED                        = 10008;
-static const int SPORK_10_MASTERNODE_PAY_UPDATED_NODES              = 10009;
-static const int SPORK_12_RECONSIDER_BLOCKS                         = 10011;
-static const int SPORK_13_OLD_SUPERBLOCK_FLAG                       = 10012;
-static const int SPORK_14_UNKNOW                                    = 10013;
+static const int SPORK_2_INSTANTSEND_ENABLED                                = 10001;
+static const int SPORK_3_INSTANTSEND_BLOCK_FILTERING                        = 10002;
+static const int SPORK_5_INSTANTSEND_MAX_VALUE                              = 10004;
+static const int SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT                     = 10007;
+static const int SPORK_9_SUPERBLOCKS_ENABLED                                = 10008;
+static const int SPORK_10_MASTERNODE_PAY_UPDATED_NODES                      = 10009;
+static const int SPORK_12_RECONSIDER_BLOCKS                                 = 10011;
+static const int SPORK_13_OLD_SUPERBLOCK_FLAG                               = 10012;
+static const int SPORK_14_UNKNOW                                            = 10013;
 // FXTC BEGIN
-static const int SPORK_MEGACOIN_01_HANDBRAKE_HEIGHT                      = 94680010;
-static const int SPORK_MEGACOIN_01_HANDBRAKE_FORCE_SHA256D               = 94680011;
-static const int SPORK_MEGACOIN_01_HANDBRAKE_FORCE_SCRYPT                = 94680012;
-static const int SPORK_MEGACOIN_01_HANDBRAKE_FORCE_NIST5                 = 94680013;
-static const int SPORK_MEGACOIN_01_HANDBRAKE_FORCE_LYRA2Z                = 94680014;
-static const int SPORK_MEGACOIN_01_HANDBRAKE_FORCE_X11                   = 94680015;
-static const int SPORK_MEGACOIN_01_HANDBRAKE_FORCE_X16R                  = 94680016;
+static const int SPORK_MEGACOIN_01_HANDBRAKE_HEIGHT                         = 94680010;
+static const int SPORK_MEGACOIN_01_HANDBRAKE_FORCE_SHA256D                  = 94680011;
+static const int SPORK_MEGACOIN_01_HANDBRAKE_FORCE_SCRYPT                   = 94680012;
+static const int SPORK_MEGACOIN_01_HANDBRAKE_FORCE_NIST5                    = 94680013;
+static const int SPORK_MEGACOIN_01_HANDBRAKE_FORCE_LYRA2Z                   = 94680014;
+static const int SPORK_MEGACOIN_01_HANDBRAKE_FORCE_X11                      = 94680015;
+static const int SPORK_MEGACOIN_01_HANDBRAKE_FORCE_X16R                     = 94680016;
 
-static const int SPORK_MEGACOIN_15_COINBASE_MATURITY_STAGE_2       = 94680021;
-static const int SPORK_MEGACOIN_02_IGNORE_EXTRA_REWARD_CHECK           = 94680022;
-static const int SPORK_MEGACOIN_02_IGNORE_EXTRA_REWARD_VALUE           = 94680023;
-static const int SPORK_MEGACOIN_16_MIN_PEER_PROTO_VERSION        = 70002;
-static const int SPORK_MEGACOIN_17_SHOW_UPDATE_MESSAGE        = 94680025;
-static const int SPORK_MEGACOIN_18_FACTOR_ENFORCEMENT        = 94680026;
-static const int SPORK_MEGACOIN_19_UNKNOW        = 94680027;
-static const int SPORK_MEGACOIN_20_UNKNOW        = 94680028;
-static const int SPORK_MEGACOIN_21_UNKNOW        = 94680031;
-static const int SPORK_MEGACOIN_22_UNKNOW        = 94680032;
+static const int SPORK_MEGACOIN_15_COINBASE_MATURITY_STAGE_2                = 94680021;
+static const int SPORK_MEGACOIN_02_IGNORE_EXTRA_REWARD_CHECK                = 94680022;
+static const int SPORK_MEGACOIN_02_IGNORE_EXTRA_REWARD_VALUE                = 94680023;
+static const int SPORK_MEGACOIN_16_MIN_PEER_PROTO_VERSION                   = 70002;
+static const int SPORK_MEGACOIN_17_SHOW_UPDATE_MESSAGE                      = 94680025;
+static const int SPORK_MEGACOIN_18_FACTOR_ENFORCEMENT                       = 94680026;
+static const int SPORK_MEGACOIN_19_UNKNOW                                   = 94680027;
+static const int SPORK_MEGACOIN_20_UNKNOW                                   = 94680028;
+static const int SPORK_MEGACOIN_21_UNKNOW                                   = 94680031;
+static const int SPORK_MEGACOIN_22_UNKNOW                                   = 94680032;
 
 // FXTC END
 
-static const int64_t SPORK_2_INSTANTSEND_ENABLED_DEFAULT                = 0;            // ON
-static const int64_t SPORK_3_INSTANTSEND_BLOCK_FILTERING_DEFAULT        = 0;            // ON
-static const int64_t SPORK_5_INSTANTSEND_MAX_VALUE_DEFAULT              = 1000;         // 1000 FxTC
-static const int64_t SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT_DEFAULT     = 4070908800ULL;// OFF
-static const int64_t SPORK_9_SUPERBLOCKS_ENABLED_DEFAULT                = 4070908800ULL;// OFF
-static const int64_t SPORK_10_MASTERNODE_PAY_UPDATED_NODES_DEFAULT      = 4070908800ULL;// OFF
-static const int64_t SPORK_12_RECONSIDER_BLOCKS_DEFAULT                 = 0;            // 0 BLOCKS
-static const int64_t SPORK_13_OLD_SUPERBLOCK_FLAG_DEFAULT               = 4070908800ULL;// OFF
-static const int64_t SPORK_14_UNKNOWG_DEFAULT                                 = 4070908800ULL;// OFF
+static const int64_t SPORK_2_INSTANTSEND_ENABLED_DEFAULT                    = 0;            // ON
+static const int64_t SPORK_3_INSTANTSEND_BLOCK_FILTERING_DEFAULT            = 0;            // ON
+static const int64_t SPORK_5_INSTANTSEND_MAX_VALUE_DEFAULT                  = 1000;         // 1000 FxTC
+static const int64_t SPORK_8_MASTERNODE_PAYMENT_ENFORCEMENT_DEFAULT         = 4070908800ULL;// OFF
+static const int64_t SPORK_9_SUPERBLOCKS_ENABLED_DEFAULT                    = 4070908800ULL;// OFF
+static const int64_t SPORK_10_MASTERNODE_PAY_UPDATED_NODES_DEFAULT          = 4070908800ULL;// OFF
+static const int64_t SPORK_12_RECONSIDER_BLOCKS_DEFAULT                     = 0;            // 0 BLOCKS
+static const int64_t SPORK_13_OLD_SUPERBLOCK_FLAG_DEFAULT                   = 4070908800ULL;// OFF
+static const int64_t SPORK_14_UNKNOW_DEFAULT                                = 4070908800ULL;// OFF
 // FXTC BEGIN
 static const int64_t SPORK_MEGACOIN_01_HANDBRAKE_HEIGHT_DEFAULT             = 4070908800ULL;// OFF
 static const int64_t SPORK_MEGACOIN_01_HANDBRAKE_FORCE_SHA256D_DEFAULT      = 1;            // 1x
@@ -80,16 +80,16 @@ static const int64_t SPORK_MEGACOIN_01_HANDBRAKE_FORCE_LYRA2Z_DEFAULT       = 1;
 static const int64_t SPORK_MEGACOIN_01_HANDBRAKE_FORCE_X11_DEFAULT          = 1;            // 1x
 static const int64_t SPORK_MEGACOIN_01_HANDBRAKE_FORCE_X16R_DEFAULT         = 1;            // 1x
 
-static const int64_t SPORK_MEGACOIN_15_COINBASE_MATURITY_STAGE_2_DEFAULT  = 4070908800ULL;// OFF
-static const int64_t SPORK_MEGACOIN_02_IGNORE_EXTRA_REWARD_CHECK_DEFAULT      = 4070908800ULL;// OFF
-static const int64_t SPORK_MEGACOIN_02_IGNORE_EXTRA_REWARD_VALUE_DEFAULT      = 4070908800ULL;// OFF
-static const int64_t SPORK_MEGACOIN_16_MIN_PEER_PROTO_VERSION_DEFAULT   = 4070908800ULL;// OFF
-static const int64_t SPORK_MEGACOIN_17_SHOW_UPDATE_MESSAGE_DEFAULT   = 4070908800ULL;// OFF
-static const int64_t SPORK_MEGACOIN_18_FACTOR_ENFORCEMENT_DEFAULT   = 4070908800ULL;// OFF
-static const int64_t SPORK_MEGACOIN_19_UNKNOW_DEFAULT   = 4070908800ULL;// OFF
-static const int64_t SPORK_MEGACOIN_20_UNKNOW_DEFAULT   = 4070908800ULL;// OFF
-static const int64_t SPORK_MEGACOIN_21_UNKNOW_DEFAULT   = 4070908800ULL;// OFF
-static const int64_t SPORK_MEGACOIN_22_UNKNOW_DEFAULT   = 4070908800ULL;// OFF
+static const int64_t SPORK_MEGACOIN_15_COINBASE_MATURITY_STAGE_2_DEFAULT    = 4070908800ULL;// OFF
+static const int64_t SPORK_MEGACOIN_02_IGNORE_EXTRA_REWARD_CHECK_DEFAULT    = 4070908800ULL;// OFF
+static const int64_t SPORK_MEGACOIN_02_IGNORE_EXTRA_REWARD_VALUE_DEFAULT    = 4070908800ULL;// OFF
+static const int64_t SPORK_MEGACOIN_16_MIN_PEER_PROTO_VERSION_DEFAULT       = 4070908800ULL;// OFF
+static const int64_t SPORK_MEGACOIN_17_SHOW_UPDATE_MESSAGE_DEFAULT          = 4070908800ULL;// OFF
+static const int64_t SPORK_MEGACOIN_18_FACTOR_ENFORCEMENT_DEFAULT           = 4070908800ULL;// OFF
+static const int64_t SPORK_MEGACOIN_19_UNKNOW_DEFAULT                       = 4070908800ULL;// OFF
+static const int64_t SPORK_MEGACOIN_20_UNKNOW_DEFAULT                       = 4070908800ULL;// OFF
+static const int64_t SPORK_MEGACOIN_21_UNKNOW_DEFAULT                       = 4070908800ULL;// OFF
+static const int64_t SPORK_MEGACOIN_22_UNKNOW_DEFAULT                       = 4070908800ULL;// OFF
 // FXTC END
 
 extern std::map<uint256, CSporkMessage> mapSporks;
